566430c18e26b49cb36e558afe5a055c53cb85b4
Mzee1991 <yohanamariamzee@gmail.com>
2022-03-11 11:55:28 -0800

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(C);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
