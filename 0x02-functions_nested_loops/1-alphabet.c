11b09a1a698618d3bb876704407c76a4e8c437b7
Mzee1991 <yohanamariamzee@gmail.com>
2022-03-15 09:30:11 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower case
+ *
+ * Return: Always(Success)
+ */
+
+int main(void)
+{
+	char c;
+	for (c ='a' c <= 'z' c++)
+	{
+		putchar(c);
+	}
+	putchar('\');
+
+	return (0);
+}
+
