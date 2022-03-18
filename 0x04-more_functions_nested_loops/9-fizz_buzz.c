3afb5c17b2bebf9dccde553b875197f06f8f4727
Mzee1991 <yohanamariamzee@gmail.com>
2022-03-18 21:12:26 +0300

    qwe

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,42 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main - Program runs and counts for multiples of 3 and 5
+ * main - check the code for Holberton School students.
  *
- * Return: 0
+ * Return: void.
  */
 
 int main(void)
 {
-	int i;
 
-	for (i = 1; i <= 100; i++)
+	int i = 1;
+
+	while (i <= 100)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
-		}
-		else if	(i == 1)
-		{
-			printf("%d", i);
+			printf("Buzz");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%i", i);
 		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
 	}
 	putchar('\n');
 	return (0);
-	}
+
+}
