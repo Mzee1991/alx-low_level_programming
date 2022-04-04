0fce239b2faef86d5637e2a2cdb45b387999a91b
Mzee1991 <yohanamariamzee@gmail.com>
2022-04-04 03:15:37 -0700

    malloc_free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,38 +1,17 @@
 #include <stdlib.h>
 
 /**
- * **alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: int
+ * free_grid - free the grid
+ * @grid: grid of memories
  * @height: int
- * Return: 2d array
+ * Return: void
  */
 
-int **alloc_grid(int width, int height)
+void free_grid(int **grid, int height)
 {
-int **tab, i, j;
-tab = malloc(sizeof(*tab) * height);
+	int i = 0;
 
-if (width <= 0 || height <= 0 || tab == 0)
-	return (NULL);
-
-else
-{
-	for (i = 0; i < height; i++)
-	{
-		tab[i] = malloc(sizeof(**tab) * width);
-		if (tab[i] == 0)
-		{
-			/*Free everything if malloc fails*/
-			while (i--)
-				free(tab[i]);
-			free(tab);
-			return (NULL);
-		}
-
-		for (j = 0; j < width; j++)
-			tab[i][j] = 0;
-	}
-}
-
-return (tab);
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
