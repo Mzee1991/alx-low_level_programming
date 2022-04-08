734ef88b9f721644f1a8c9973106ba20e022aea1
Mzee1991 <yohanamariamzee@gmail.com>
2022-04-08 01:30:04 -0700

    asd

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,7 +7,7 @@ int is_digit(char c);
 int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
 void print_me(int *sum_result, int len_r);
 int _putchar(char c);
-void *malloc_checked(size_t b);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
