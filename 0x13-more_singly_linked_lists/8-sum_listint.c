9d3abd35563185e3fd41e74540aa0c4bf8b2d705
Mzee1991 <yohanamariamzee@gmail.com>
2022-04-25 08:57:34 -0700

    little changes

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -11,13 +11,12 @@
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-
-	if (head == NULL)
-		return (0);
 	listint_t *ptr = NULL;
 
 	ptr = head;
 
+	if (head == NULL)
+		return (0);
 	while (ptr != NULL)
 	{
 		sum += ptr->n;
