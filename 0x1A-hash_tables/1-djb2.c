5c00d064f10b8d01308334cf348bf76216b8d1e6
Mzee1991 <yohanamariamzee@gmail.com>
2022-07-01 14:19:43 +0300

    hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,17 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - hash function generates a djb2
- * @str: pointer to chars a hash
- * Return: hash key
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string to hash
+ *
+ * Return: hash index corresponding to the string
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long hash = 5381;
+	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
-	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
